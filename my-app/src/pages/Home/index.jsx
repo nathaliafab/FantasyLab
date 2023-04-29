94540b719ff53871b8eb1c6dfaabd43ffa80ba6b
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-29 11:52:16 -0300

    add css paia

--- my-app/src/pages/Home/index.jsx
+++ my-app/src/pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
     return (
-        <div className="App">
+        <div className="Home">
             <h1>Welcome</h1>
             <div className="button-container">
                 <Link to="/character">
