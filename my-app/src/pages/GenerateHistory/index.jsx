1480d40713736a083e8c2d393324586e714f74cd
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-29 15:48:19 -0300

    update generate history

--- my-app/src/pages/GenerateHistory/index.jsx
+++ my-app/src/pages/GenerateHistory/index.jsx
@@ -7,9 +7,19 @@ function GenerateHistory() {
     const [isLoading, setIsLoading] = useState(false);
     const [ambiance, setAmbiance] = useState('');
     const [theme, setTheme] = useState('');
+    const [otherInfo, setOtherInfo] = useState('');
+    const [charNames, setCharNames] = useState('');
 
     const generatePrompt = () => {
-        return `Write a RPG history with the following ambiance: ${ambiance}, theme: ${theme}.`;
+        let prompt = `Write a RPG history with the following ambiance: ${ambiance}, theme: ${theme}.`;
+
+        if (charNames !== '')
+            prompt += ` The history may have the following characters: ${charNames}.`;
+
+        if (otherInfo !== '')
+            prompt += ` ${otherInfo}`;
+
+        return prompt;
     }
 
     const generateText = async () => {
@@ -17,7 +27,7 @@ function GenerateHistory() {
         const { Configuration, OpenAIApi } = require("openai");
 
         const configuration = new Configuration({
-            apiKey: process.env.OPENAI_API_KEY,
+            apiKey: "",
         });
         const openai = new OpenAIApi(configuration);
 
@@ -44,6 +54,16 @@ function GenerateHistory() {
             <div>
                 <label>Theme:</label>
                 <input type="text" value={theme} onChange={e => setTheme(e.target.value)} required />
+            </div>
+
+            <div>
+                <label>Character names (separate by commas):</label>
+                <input type="text" value={charNames} onChange={e => setCharNames(e.target.value)} />
+            </div>
+
+            <div>
+                <label>Other information:</label>
+                <input type="text" value={otherInfo} onChange={e => setOtherInfo(e.target.value)} />
             </div>
 
             <button onClick={generateText}>Generate history</button>
