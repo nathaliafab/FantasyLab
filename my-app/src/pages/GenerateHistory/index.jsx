4b4cc6da1a62d518e8e8a21137296c40b4963613
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-29 12:18:25 -0300

    Add chamada funcional pra gpt 3.5

--- my-app/src/pages/GenerateHistory/index.jsx
+++ my-app/src/pages/GenerateHistory/index.jsx
@@ -2,11 +2,41 @@ import React, { useState } from "react";
 import 'openai';
 
 function GenerateHistory() {
-  return (
-    <div className="App">
-      <p>Oie</p>
-    </div>
-  );
+    const [history, setHistory] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const generatePrompt = () => {
+        return `Write a RPG fantasy history that I can work with.`;
+    }
+
+    const generateText = async () => {
+        setIsLoading(true);
+        const { Configuration, OpenAIApi } = require("openai");
+
+        const configuration = new Configuration({
+            apiKey: "",
+        });
+        const openai = new OpenAIApi(configuration);
+
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: generatePrompt() }],
+        });
+
+        setIsLoading(false);
+        const arr = Object.entries(completion.data);
+        console.log(arr[5][1][0].message.content);
+
+        setHistory(arr[5][1][0].message.content);
+    };
+
+    return (
+        <div className="App">
+            <button onClick={generateText}>Generate history</button>
+            {isLoading && <p>Loading...</p>}
+            {!isLoading && history && <p>{history}</p>}
+        </div>
+    );
 }
 
 export default GenerateHistory;
